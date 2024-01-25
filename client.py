9282967031086d37f8fd8343150fe9b6090f4c19
Cody Chen <cdxvy30@gmail.com>
2024-01-25 15:54:25 +0800

    remove runnable interface

--- client.py
+++ client.py
@@ -2,13 +2,6 @@ from langserve import RemoteRunnable
 from langchain_core.messages import AIMessage, HumanMessage
 import requests
 
-
-# remote_chain = RemoteRunnable("http://localhost:8000/agent/")
-# response = remote_chain.invoke({
-#   "input": "你好嗎？",
-#   "chat_history": []
-# })
-
 response = requests.post("http://localhost:8000/query/", json={
   "input": "我叫什麼名字？",
   "chat_history": ["你好，我叫何宏發", "你好"],
