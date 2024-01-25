cde1a1db01039a48be6e8b0b897e0ac6937917de
peihsin0715 <peihsin0715@gmail.com>
2024-01-25 16:54:53 +0800

    adding chat history and model exchange

--- client.py
+++ client.py
@@ -1,9 +1,25 @@
-from langserve import RemoteRunnable
+import requests
 
-remote_chain = RemoteRunnable("http://localhost:8000/agent/")
-response = remote_chain.invoke({
-  "input": "how did DARPA internet protocols design?",
-  "chat_history": []
-})
+chat_history = []
+def send_request(input_text):
+    global chat_history 
+    request_data = {
+        "input": input_text,
+        "model": "mistralai/Mixtral-8x7B-Instruct-v0.1",
+        "chat_history": chat_history
+    }
 
-print(response)
+    response = requests.post("http://localhost:8000/agent/", json=request_data)
+    response_data = response.json()
+    chat_history.append({
+        "input": input_text,
+        "response": response_data.get("answer"),
+    })
+
+    print("Answer:", response_data.get("answer"))
+    print("Reference 1:", response_data.get("reference1"))
+    print("Reference 2:", response_data.get("reference2"))
+    print("Chat History:", chat_history)
+
+send_request("工作場所的高度高於多少需要定義墜落災害防止計畫？")
+send_request("有哪些基本的墜落防護措施？")
--- example.py
+++ example.py
@@ -3,75 +3,72 @@ from dotenv import load_dotenv
 import os
 
 from fastapi import FastAPI
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langchain_community.document_loaders import WebBaseLoader
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_openai import OpenAIEmbeddings
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.tools.retriever import create_retriever_tool
-from langchain_community.tools.tavily_search import TavilySearchResults
-from langchain_openai import ChatOpenAI
-from langchain import hub
-from langchain.agents import create_openai_functions_agent
-from langchain.agents import AgentExecutor
-from langchain.pydantic_v1 import BaseModel, Field
-from langchain_core.messages import BaseMessage
-from langserve import add_routes
+from langchain import HuggingFaceHub
+from langchain.chains.question_answering import load_qa_chain
 
-from src.utils.io import Input, Output
+from utils.io import ChatRequest
+from pydantic import BaseModel
 
 load_dotenv()
 
-# 1. Load Retriever (PDF)
-loader = PyPDFLoader('./sample/The Design Philosophy of the DARPA Internet Protocols.pdf')
-pages = loader.load_and_split()
-embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
-faiss_index = FAISS.from_documents(pages, embeddings)
-retriever = faiss_index.as_retriever()
-# docs = faiss_index.similarity_search("What is the design philosophy of DARPA Internet Protocols?", k=2)
-# for doc in docs:
-#   print(str(doc.metadata["page"]) + ":", doc.page_content[:100])
-
-# Web Base Load Example
-# loader = WebBaseLoader("https://docs.smith.langchain.com/overview")
-# docs = loader.load()
-# text_splitter = RecursiveCharacterTextSplitter()
-# documents = text_splitter.split_documents(docs)
-# embeddings = OpenAIEmbeddings(openai_api_key=os.getenv("OPENAI_API_KEY"))
-# vector = FAISS.from_documents(documents, embeddings)
-# retriever = vector.as_retriever()
-
-# 2. Create Tools
-retriever_tool = create_retriever_tool(
-  retriever,
-  "internet_protocols_search",
-  "Search for information about Internet Protocols. For any questions about Internet Protocols, you must use this tool!",
-)
-search = TavilySearchResults()
-tools = [retriever_tool, search]
-
-
-# 3. Create Agent
-prompt = hub.pull("hwchase17/openai-functions-agent")
-llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-agent = create_openai_functions_agent(llm, tools, prompt)
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-
-
-# 4. App definition
 app = FastAPI(
   title="LangChain Server",
   version="1.0",
   description="A simple API server using LangChain's Runnable interfaces",
 )
 
-add_routes(
-  app,
-  agent_executor.with_types(input_type=Input, output_type=Output),
-  path="/agent",
-)
+class ChatHistoryItem(BaseModel):
+    input: str
+    response: str
+
+class ChatRequest(BaseModel):
+    input: str
+    model: str
+    chat_history: List[ChatHistoryItem]
+
+@app.post("/agent/")
+async def agent(request: ChatRequest) -> dict:
+  chat_history = request.chat_history
+  folder_path = './samples'
+  document = []
+  for file in os.listdir(folder_path):
+      if file.endswith('.pdf'):
+          file_path = os.path.join(folder_path, file)
+          loader = UnstructuredPDFLoader(file_path)
+          document.extend(loader.load())
+  text_spilter = RecursiveCharacterTextSplitter(chunk_size = 500, chunk_overlap=300, separators=[" ", ",", "\n"])
+  pages = text_spilter.split_documents(document)
+  embeddings = HuggingFaceEmbeddings()
+  faiss_index = FAISS.from_documents(pages, embeddings)
+
+  os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+  llm = HuggingFaceHub(
+    repo_id=request.model,
+    model_kwargs={"temperature":0.2, "max_length":1500},
+  )
+  chain = load_qa_chain(llm, chain_type="stuff")
+  """Handle a request."""
+  #print(request.input)
+  docs = faiss_index.similarity_search(request.input, k=2)
+  res = chain.run(input_documents=docs, question=request.input, chat_history=chat_history)
+  #print(res)
+  reference1 = docs[0].page_content
+  reference2 = docs[1].page_content
+  response_data = {
+        "answer": res,
+        "reference1": reference1,
+        "reference2": reference2
+  }
+
+  return response_data
+
+
 
 if __name__ == "__main__":
   import uvicorn
