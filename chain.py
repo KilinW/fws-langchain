f651e964d22eaa225d8dbccae1a14b9bc4eb04a6
Cody Chen <cdxvy30@gmail.com>
2024-01-25 18:07:47 +0800

    move prompt to I/O module

--- chain.py
+++ chain.py
@@ -2,20 +2,8 @@ from langchain import hub, HuggingFaceHub
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import LLMChain
-from langchain_core.prompts import PromptTemplate
 from langchain.chains.question_answering import load_qa_chain
-
-
-prompt = PromptTemplate(
-  input_variables = ["input", "chat_history", "retrieved_document"],
-   template="\
-    你是一個場務知識的聊天機器人，你擅長根據Context和Chat History回答問題，\
-    以下是Context、Chat History和問題，請你只針對該問題回答。\n\n\
-    Context: {retrieved_document} \n\n\
-    Chat History:\n{chat_history}\n\n\
-    Question:{input} \n\
-    Answer:",
-)
+from utils.io import prompt
 
 def get_openai_llm(model: str="gpt-3.5-turbo"):
   return ChatOpenAI(model=model, temperature=0)
--- utils/io.py
+++ utils/io.py
@@ -1,6 +1,19 @@
 from langchain.memory import ChatMessageHistory
+from langchain_core.prompts import PromptTemplate
 from pydantic import BaseModel, Field
 from typing import List
+
+
+prompt = PromptTemplate(
+  input_variables = ["input", "chat_history", "retrieved_document"],
+   template="\
+    你是一個場務知識的聊天機器人，你擅長根據Context和Chat History回答問題，\
+    以下是Context、Chat History和問題，請你只針對該問題回答。\n\n\
+    Context: {retrieved_document} \n\n\
+    Chat History:\n{chat_history}\n\n\
+    Question:{input} \n\
+    Answer:",
+)
 
 
 class ModelParams(BaseModel):
